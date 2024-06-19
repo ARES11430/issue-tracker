254a0aed0adec3f0bcf9d14951b76c91a429069d
Amin Ghasemi <amin11.ghasemi@gmail.com>
2024-06-19 14:01:55 +0330

    added provider to next auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,19 @@
 import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import prisma from '@/prisma/db';
 
 const handler = NextAuth({
-	providers: []
+	adapter: PrismaAdapter(prisma),
+	session: {
+		strategy: 'jwt'
+	},
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID!,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+		})
+	]
 });
 
 export { handler as GET, handler as POST };
