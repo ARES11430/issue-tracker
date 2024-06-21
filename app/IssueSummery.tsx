85070c34eda8bb859164d5af73133c6163d33388
Amin Ghasemi <amin11.ghasemi@gmail.com>
2024-06-21 14:05:16 +0330

    fix bug for summery

--- app/IssueSummery.tsx
+++ app/IssueSummery.tsx
@@ -29,7 +29,7 @@ const IssueSummary = ({ open, inProgress, closed }: Props) => {
 			{containers.map((container) => (
 				<Card key={container.label}>
 					<Flex direction='column' gap='1'>
-						<Link className='text-sm font-medium' href={`/issues/list?status=${container.status}`}>
+						<Link className='text-sm font-medium' href={`/issues?status=${container.status}`}>
 							{container.label}
 						</Link>
 						<Text size='5' className='font-bold'>
